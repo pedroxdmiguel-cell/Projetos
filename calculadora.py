7bd4f91850e4ea7fc3d51b22ee03b9026258f432
pedroalves <pedroxdmiguel@gmail.com>
2026-06-19 17:16:04 -0300

    projetos

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,24 @@
-print ("hello world")
+
+num1 = 0
+num2 = 0
+result = 0
+op     = ''
+
+while True:
+    num1 = float(input('Digite o primeiro núemro: '))
+    num2 = float(input('Digite o primeiro núemro: '))
+    op   = input('Digite a operação a ser feita: ')
+
+    if op == ' ':
+        result = num1 + num2
+    elif op == '-':
+         result = num1 - num2
+    elif op == '/':
+        result = num1 / num2
+    elif op == '*':
+        result = num1 * num2
+    else:
+        print("Comando não reconhecido")
+
+    print('{} {} {} = {}' .format(num1, op, num2, result))
+
