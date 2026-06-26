5770a0d8882dcdf121bd2a1d73ef36bb22d1acba
pedroalves <pedroxdmiguel@gmail.com>
2026-06-26 12:17:52 -0300

    calculadora de imc

--- CaluladoraIMC.py
+++ CaluladoraIMC.py
@@ -1,17 +1,18 @@
 num1 = float(input("Digite sua altura: "))
+
 num2 = float(input("Digite seu peso: " ))
 
-imc = (num1 * 2 ) / num2
+imc = num2 / (num1 ** 2 ) 
 
-if imc >= 18:
+if imc < 18:
     print("Abaixo do peso")
-elif imc <= 25:
+elif imc < 25:
     print("Peso normal")
-elif imc <= 30:
+elif imc < 30:
     print("Sobrepeso")
-elif imc <= 35:
+elif imc < 35:
     print("Obesidade grau 1")
-elif imc <= 39:
+elif imc < 39:
     print("Obesidade grau 2")
 else:
-    print("Obseidade grau 3")
+    print("Obseidade grau 3") 
